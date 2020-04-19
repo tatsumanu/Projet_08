3665781f5a345d192114be8a7bd5ffb346278981
tatsumanu <55323814+tatsumanu@users.noreply.github.com>
2020-04-19 12:37:07 +0200

    heroku 4

--- Projet_08/Projet_08/settings.py
+++ Projet_08/Projet_08/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
