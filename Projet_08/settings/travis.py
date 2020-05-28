02fe58e74798fc0f8e2c769c323c98289a18d49a
root <root@ns329147.ip-37-187-116.eu>
2020-05-28 15:57:08 +0000

    changed .travis.yml & settings.travis

--- Projet_08/settings/travis.py
+++ Projet_08/settings/travis.py
@@ -3,14 +3,11 @@ from .base import *
 DATABASES = {
     'default': {
 	 'ENGINE': 'django.db.backends.postgresql',
-	 'NAME': 'postgres',
+	 'NAME': '',
 	 'USER': 'postgres',
 	 'PASSWORD': '',
 	 'HOST': '',
 	 'PORT': '',
-	 'OPTIONS': {
-            'isolation_level': psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE,
-        },
     },
 }
 
