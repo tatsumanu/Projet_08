6367a984a18a20b3640051cb9830981c53ef5852
Manu <manu@ns329147.ip-37-187-116.eu>
2020-06-11 09:05:44 +0000

    changed deploy3

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd Projet_08
 git pull
 python3.8 manage.py collectstatic --noinput --settings=Projet_08.settings.production
 python3.8 manage.py migrate --settings=Projet_08.settings.production
-/usr/bin/supervisorctl restart projet_08-gunicorn
+sudo /usr/bin/supervisorctl restart projet_08-gunicorn
 
