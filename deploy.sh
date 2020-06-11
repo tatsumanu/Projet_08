cd8b4fb459ed15f13aaa61fb939ffbe796501967
Manu <manu@ns329147.ip-37-187-116.eu>
2020-06-11 08:41:12 +0000

    changed deploy2

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ source env/bin/activate
 cd Projet_08
 git pull
 python3.8 manage.py collectstatic --noinput --settings=Projet_08.settings.production
-python3.8 manage.py migrate
+python3.8 manage.py migrate --settings=Projet_08.settings.production
 /usr/bin/supervisorctl restart projet_08-gunicorn
 
